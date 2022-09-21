243c75d6c5b9582606d5aba5c74addb9058340d5
muhammeds <muhammed.shehab@cellwize.com>
2022-09-21 12:01:07 +0300

    clean up

---
+++ src/main/java/com/chime/developer/Main.java
@@ -0,0 +1,15 @@
+package com.chime.developer;
+
+import com.cellwize.developersdk.AppConfig;
+import com.cellwize.developersdk.ApplicationInstance;
+import com.cellwize.developersdk.IApplication;
+
+
+public class Main {
+    public static void main(String[] args) {
+        // AppConfig config = new AppConfig.Builder().build();
+        // IApplication app = ApplicationInstance.newInstance(config);
+
+        System.out.println("-------------------------- CHIME DEVELOPER --------------------------");
+    }
+}
